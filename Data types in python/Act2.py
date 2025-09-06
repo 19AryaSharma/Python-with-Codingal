4beed08c0dcd1f521339f30ec9c4a42492e3f16a
19AryaSharma <arya2012sharma@gmail.com>
2025-09-06 17:45:34 +0530

    Add initial Python scripts for data types demonstration and string reversal

---
+++ Data types in python/Act2.py
@@ -0,0 +1,24 @@
+name = "pengiun"
+age = 13
+Is_student = True
+weight = 43.5
+
+print("name : ", name)
+print("Data type of name : ", type(name))
+
+print("age : ", age)
+print("Data type of age : ", type(age))
+
+print("Is_student : ", Is_student)
+print("Data type of Is_student : ", type(Is_student))
+
+print("weight : ", weight)
+print("Data type of weight : ", type(weight))
+
+print("n\ After type Casting... ")
+age = str(age)
+print(age)
+print("Data type of age : ", type(age))
+weight = int(weight)
+print(weight)
+print("Data type of weight : ", type(weight))
