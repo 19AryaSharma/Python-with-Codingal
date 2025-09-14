3cb929f76ea9bc81d34ee7f8e9b0975a2a6cc7bf
19AryaSharma <arya2012sharma@gmail.com>
2025-09-14 17:18:52 +0530

    Add Act1.py, Act2.py, and Act3.py scripts for basic operator demonstrations and BMI calculation

---
+++ Operator II/Act1.py
@@ -0,0 +1,27 @@
+a = 10
+b = 12
+c = 0
+
+if a and b and c:
+    print("All the numbers have boolean value True")
+
+else:
+    print("At least one of the numbers have boolean value False")
+
+
+
+    a = 10
+    b = -10
+    c = 0
+
+    if a > 0 or b > 0:
+        print("either of the number is greater than 0")
+
+    else:
+        print("both the numbers are less than or equal to 0")
+
+        if b > c or c > 0:
+            print("either of the number is greater than 0")
+
+        else:
+            print("both the numbers are less than or equal to 0")
---
+++ Operator II/Act2.py
@@ -0,0 +1,26 @@
+a = 10 
+b = 12
+c = 12
+print ( a != b)
+print ( a != c)
+
+
+a = "Coding"
+b = "python"
+
+if a != b:
+    print(a,"and",b,"are different")
+
+
+
+a = 4
+b = 5
+
+if (a == 1) != (b==5):
+    print("hello")
+
+
+    a = int(input("enter the number:"))
+
+    if a%2 != 0:
+        print(a,"is odd number")
