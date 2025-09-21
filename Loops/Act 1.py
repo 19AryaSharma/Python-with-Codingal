99bd7f1e38fa7a193fceac464ee600a6ccf33f94
19AryaSharma <arya2012sharma@gmail.com>
2025-09-21 17:15:27 +0530

    ddd

---
+++ Loops/Act 1.py
@@ -0,0 +1,6 @@
+n = int(input("Enter the number whose sum do you want to Find: "))
+sum = 1
+
+for i in range(1, n + 1):
+    sum = sum + i
+    print("\n sum=", sum )
---
+++ Loops/string reverse.py
@@ -0,0 +1,7 @@
+string = str(input("Enter your own string: "))
+string2 = ""
+for i in string :
+    string2 = i + string2 
+
+    print("\n the original string =", string)
+    print("the reversed string =", string2)
